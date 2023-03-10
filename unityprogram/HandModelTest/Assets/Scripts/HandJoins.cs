eccbecdcab99512752c2d9004145ffe8a1c98089
nightelf <guolicheng800@163.com>
2023-03-10 19:05:04 +0800

    更新main分支内容

--- unityprogram/HandModelTest/Assets/Scripts/HandJoins.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class HandJoins : MonoBehaviour
-{
-    public Vector3 hand_root;
-    public Vector3[] handjoint;
-}
